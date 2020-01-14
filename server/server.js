2810eca26e55c2af139b555901b9757b6c0010b8
unknown <zkazi986@gmail.com>
2020-01-14 16:47:11 +0500

    init commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const port= process.env.PORT || 3000;
+const publicpath= path.join(__dirname + '../../public');
+
+
+var app=express();
+
+app.use(express.static(publicpath));
+
+app.listen(port,()=>{
+    console.log(`server is listening at ${port}`)
+})
+
+
+
